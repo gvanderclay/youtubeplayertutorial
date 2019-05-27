ad92adc98a8c891b8e70478ca8a6231b204cd09e
Gage Vander Clay <gvanderclay@gmail.com>
2019-05-27 19:21:15 -0400

    add video through xml

--- app/src/main/java/gage/vanderclay/youtubeplayertutorial/MainActivity.kt
+++ app/src/main/java/gage/vanderclay/youtubeplayertutorial/MainActivity.kt
@@ -1,12 +1,40 @@
 package gage.vanderclay.youtubeplayertutorial
 
+import android.app.Activity
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerFragment
+import com.google.android.youtube.player.YouTubePlayerSupportFragment
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), YouTubePlayer.OnInitializedListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        val youTubePlayerFragment = supportFragmentManager.findFragmentById(R.id.youtube_fragment) as YouTubePlayerSupportFragment;
+        youTubePlayerFragment.initialize("dummy", this);
     }
+
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
+        if(player == null) return
+
+        if(wasRestored) {
+            player.play()
+        } else {
+            player.cueVideo("aYQBvnWRBUw")
+            player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+        }
+    }
+
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider?, result: YouTubeInitializationResult?) {
+        Log.d("Youtube Player", "Failed to initialize");
+    }
+
 }
