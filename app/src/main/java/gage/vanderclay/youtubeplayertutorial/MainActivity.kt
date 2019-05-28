89e31ab1625bfac0cd4ea907f161c4d684a3933a
Gage Vander Clay <gvanderclay@gmail.com>
2019-05-28 19:45:56 -0400

    Remove irrelevant dependencies

--- app/src/main/java/gage/vanderclay/youtubeplayertutorial/MainActivity.kt
+++ app/src/main/java/gage/vanderclay/youtubeplayertutorial/MainActivity.kt
@@ -1,17 +1,10 @@
 package gage.vanderclay.youtubeplayertutorial
 
-import android.app.Activity
-import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
-import android.support.v4.app.FragmentManager
 import android.util.Log
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerFragment
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
 
 class MainActivity : AppCompatActivity(), YouTubePlayer.OnInitializedListener  {
